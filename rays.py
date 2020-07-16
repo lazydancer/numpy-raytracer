9c03f8289e74b75b5a7dbf9979847e845d7e9618
lazydancer <jrpucula@gmail.com>
2020-07-16 14:06:23 -0400

    Rays updated

--- rays.py
+++ rays.py
@@ -13,11 +13,25 @@ class Metal:
         self.diffusion = diffusion
         self.emit = emit
 
+    def _random_in_unit_sphere(self, normals):
+        a = np.random.rand(normals.shape[0]) * 2 * np.pi
+        z = np.random.rand(normals.shape[0]) * 2 - 1
+        r = np.sqrt(1 - z*z)
+
+        x = r * np.cos(a)
+        y = r * np.sin(a)
+
+        unit_random = np.array([x, y, z]).T
+
+        return unit_random + normals
+
+
+
     def _reflect(self, directions, normals):
         reflect = normalize(directions - 2 * np.einsum('ij, ij->i', directions, normals)[:,None] * normals)
-        gauss = np.random.normal(size=directions.shape)
-        gauss = normalize(gauss * np.sum(gauss * normals))
-
+        #gauss = np.random.normal(size=directions.shape)
+        #gauss = normalize(gauss * np.sum(gauss * normals))
+        gauss = self._random_in_unit_sphere(normals)
         new_directions = reflect + self.diffusion * gauss
 
         return new_directions
@@ -28,30 +42,6 @@ class Metal:
 
         return origins, new_directions
 
-
-class Dielectric:
-    def __init__(self, refraction_index):
-        self.ri = refraction_index
-        self.color = np.array([1, 1, 1], dtype=np.float32)
-
-    def _refract(self, directions, normals):
-        '''
-        https://en.wikipedia.org/wiki/Snell's_law
-        '''
-        r = 1/1.4
-        normals[np.where(np.einsum('ij, ij->i', directions, -normals) < 0)] *= -1
-        c = np.einsum('ij, ij->i', directions, -normals)
-
-        return r * directions + (r * c - np.sqrt(1 - r**2 * ( 1 - c**2 )))[:,np.newaxis] * normals
-
-
-    def scatters(self, directions, normals, intersections):
-        origins = intersections - normals * 0.001
-        new_directions = self._refract(normalize(directions), normals)
-
-        return origins, new_directions
-
-
 class Sphere:
     def __init__(self, center, radius, material):
         self.center = np.array(center, dtype=np.float32)
@@ -59,31 +49,17 @@ class Sphere:
         self.material = material
 
     def hit(self, origins, directions):
-        t = np.full(directions.shape[0], np.inf)
-        temp = origins - self.center
-        a = np.sum(np.square(directions), axis=1) 
-        b = 2.0 * np.einsum('ij, ij->i', temp, directions)
-        c = np.sum(np.square(temp), axis=1) - self.radius * self.radius
-        discriminant = b * b - 4.0 *  a * c
-        possible_hits = np.where(discriminant >= 0.0)
+        m = origins - self.center
 
-        discriminant = discriminant[possible_hits]
+        b = np.einsum('ij, ij->i', m, directions)
+        c = np.einsum('ij, ij->i', m, m) - self.radius*self.radius
+        t = -b - np.sqrt(b*b - c)
 
-        distSqrt = np.sqrt(discriminant)
-        distSqrt[np.where( b[possible_hits] < 0 )] *= -1
-
-        q = (-b[possible_hits] + distSqrt) / 2.0
-
-        t0 = q / a[possible_hits] 
-        t1 = c[possible_hits] / q
-
-        roots = np.vstack([t0, t1]).T
-        roots[np.where(roots < 0)] = np.inf
-        roots.sort(axis=1)
-        
-        t[possible_hits] = roots[:,0]
+        t[np.isnan(t)] = np.inf
+        t[np.where( (t < 0) | ((c > 0) & (b > 0)) )] = np.inf
 
         return t
+
 
     def scatters(self, directions, intersections):
         normals = normalize(intersections - self.center)
@@ -99,40 +75,37 @@ def ray_color(scene, origins, directions, max_depth):
     Inputs the scene and the rays, manages the colors for each depth
     '''
     origins = origins.copy()
-    directions = directions.copy()
+    directions = normalize(directions.copy())
 
-    mask = np.ones(origins.shape[0], dtype=np.bool)
+    mask = np.arange(origins.shape[0])
     colors = np.ones(origins.shape, dtype=np.float32)
     emit = np.zeros(origins.shape[0], dtype=np.bool)
 
     for i in range(max_depth):
-        sub_objects = np.empty(mask.sum(), dtype=object)
-        ts = np.full(mask.sum(), np.inf, dtype=np.float32)
+        sub_objects = np.empty(mask.shape, dtype=object)
+        ts = np.full(mask.shape, np.inf, dtype=np.float32)
 
         for obj in scene:
-            obj_ts = obj.hit(origins[mask,:], directions[mask,:])
+            obj_ts = obj.hit(origins[mask], directions[mask])
+            is_closer = np.where(obj_ts < ts)
+            sub_objects[is_closer] = obj
+            ts[is_closer] = obj_ts[is_closer]
 
-            sub_objects[obj_ts < ts] = obj
-            ts[obj_ts < ts] = obj_ts[obj_ts < ts]
-
-        objects = np.empty(mask.shape, dtype=object)
+        objects = np.empty(origins.shape[0], dtype=object)
         objects[mask] = sub_objects
-        
-        # Filter no hits, there is no open sky in this situation so ts cannot equal np.inf 
-        # mask = ts != np.inf
 
         intersections = np.full(origins.shape, np.inf, dtype=np.float32)
-        intersections[mask] = origins[mask] + directions[mask,:] * ts[:, np.newaxis]
-
+        intersections[mask] = origins[mask] + directions[mask] * ts[:, np.newaxis]
 
         for obj in scene:
-            colors[objects == obj] *= obj.material.color
-            origins[objects == obj], directions[objects == obj] = obj.scatters(directions[objects == obj], intersections[objects == obj])
-            emit[objects == obj] = obj.material.emit
+            is_obj = np.where(objects == obj)
+            colors[is_obj] *= obj.material.color
+            origins[is_obj], directions[is_obj] = obj.scatters(directions[is_obj], intersections[is_obj])
+            emit[is_obj] = obj.material.emit
 
-        mask[emit] = False
+        mask = np.setdiff1d(mask, np.where(emit))
 
-        print(mask.sum())
+        print(i, len(mask))
 
 
     colors[mask] = 0
@@ -170,7 +143,7 @@ class Camera:
 
         self.sub_pixels_across = config['sub_pixels_across']
 
-    def take_picture(self, scene):
+    def take_picture(self, scene, save_loc):
         i = np.linspace(0, 1, self.width * self.sub_pixels_across)
         j = np.linspace(0, 1, self.height * self.sub_pixels_across)
 
@@ -193,7 +166,7 @@ class Camera:
         img = colors.reshape(self.width, self.height, 3)
         img = np.transpose(img, (1, 0, 2))
 
-        plt.imsave('out.png', img, origin='lower')
+        plt.imsave(save_loc, img, origin='lower')
 
 def main():
 
@@ -201,30 +174,78 @@ def main():
         'vup': np.array((0,1,0)),
         'vfov': np.pi/3, # In radians, give the virtical field of view
         'aspect_ratio': 16 / 9, 
-        #'aperture': 0.5, # The radius of the apetrue
-        'image_width': 700,
-        'sub_pixels_across': 2,
-        'samples_per_pixel': 1,
-        'max_depth': 300,
+        'image_width': 700, 
+        'sub_pixels_across': 7, # 7
+        'samples_per_pixel': 10, # 5
+        'max_depth': 4, 
     }
 
     camera = Camera(config, [0,1,1], [0,0,-1])
 
     scene = [
-        Sphere([0, 0 , -1], 0.3, Metal([0.2, 0.2, 0.2], 0.01)),
-        Sphere([-1, 0 , -1], 0.3, Metal([1, 1, 1], 0.5, True)),
-        Sphere([1, 0, -1], 0.3, Metal([1, 0.5, 0.4], 0.5)),
-        Sphere([0, -1000.3, -1], 1000, Metal([1, 1, 1], 0.8)), # Ground
+        Sphere([0, 0 , -1], 0.3, Metal([1, 1, 1], 0)),
+        Sphere([-1, 0 , -1], 0.3, Metal([1, 0.9, 0.9], 0.5, True)),
+        Sphere([1, 0, -1], 0.3, Metal([1, 0.6, 0.5], 0.5)),
+        Sphere([0, -1000.3, -1], 1000, Metal([1, 1, 1], 1)), # Ground
         Sphere([0, 1002, -1], 1000, Metal([1, 1, 1], 1)), # Ceiling 
-        Sphere([1002, 0, -1], 1000, Metal([0.1, 0.1, 1], 0.8)), # Right
-        Sphere([-1002, 0, -1], 1000, Metal([1, 0.1, 0.1], 0.8)), # Left
-        Sphere([0, 0, -1002], 1000, Metal([1, 0.8, 1], 0.8)), # Back
-        Sphere([0, 0, 1002], 1000, Metal([1, 1, 1], 0.8)), # Back
+        Sphere([1002, 0, -1], 1000, Metal([0.1, 0.1, 1], 1)), # Right
+        Sphere([-1002, 0, -1], 1000, Metal([1, 0.1, 0.1], 1)), # Left
+        Sphere([0, 0, -1003], 1000, Metal([1, 0.8, 1], 1)), # Back
+        Sphere([0, 0, 1002], 1000, Metal([1, 1, 1], 1)), # Back
     ]
 
-    camera.take_picture(scene)
+    camera.take_picture(scene, f'out.png')        
 
+
+def rotate():
+
+    config = {
+        'vup': np.array((0,1,0)),
+        'vfov': np.pi/3, # In radians, give the virtical field of view
+        'aspect_ratio': 16 / 9, 
+        'image_width': 700, 
+        'sub_pixels_across': 7, # 7
+        'samples_per_pixel': 1,
+        'max_depth': 5, 
+    }
+
+
+    i = 346
+    for coord in rotate_camera_around_point()[346:453]:
+        print(coord)
+
+        camera = Camera(config, coord, [0,0,-1])
+
+        scene = [
+            Sphere([0, 0 , -1], 0.3, Metal([0.2, 0.2, 0.2], 0.01)),
+            Sphere([-1, 0 , -1], 0.3, Metal([1, 1, 1], 0.5, True)),
+            Sphere([1, 0, -1], 0.3, Metal([1, 0.5, 0.4], 0.5)),
+            Sphere([0, -1000.3, -1], 1000, Metal([1, 1, 1], 0.9)), # Ground
+            Sphere([0, 1002, -1], 1000, Metal([1, 1, 1], 0.9)), # Ceiling 
+            Sphere([1002, 0, -1], 1000, Metal([0.1, 0.1, 1], 0.9)), # Right
+            Sphere([-1002, 0, -1], 1000, Metal([1, 0.1, 0.1], 0.9)), # Left
+            Sphere([0, 0, -1003], 1000, Metal([1, 0.8, 1], 0.9)), # Back
+            Sphere([0, 0, 1002], 1000, Metal([1, 1, 1], 0.9)), # Back
+        ]
+
+        camera.take_picture(scene, f'gif/{i:03d}.png')
+        i += 1
+
+# def rotate_camera_around_point():
+#     frames = 500
+#     step = 2*np.pi / frames  
+
+#     rads = np.linspace(0, 2*np.pi, frames)
+
+#     result = np.array([2*np.sin(-rads), np.ones(rads.shape), 2*np.cos(-rads)-1])
+
+#     return result.T
+
+
+#rotate_camera_around_point()
 import time
 start=time.time()
 main()
 print("time: {0:.6f}".format(time.time()-start))
+
+#"C:\Users\jpucula\Portable Apps\ffmpeg-20200522-38490cb-win64-static\bin\ffmpeg.exe" -framerate 30 -i  C:\Users\jpucula\Documents\ray_tracer_v2\gif\%03d.png C:\Users\jpucula\Documents\ray_tracer_v2\gif\output.mp4
